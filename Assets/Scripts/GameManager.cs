f5aea78d829deecab079774a08937d97107bba29
Shizounu <erik@simon-potsdam.de>
2023-01-15 16:10:36 +0100

    Init of Redo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager Instance { get; protected set; }
+    private void Awake() {
+        if(Instance != null && Instance != this){
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+
+        
+        inputActions = new();
+        runningLoadingOperations = new();
+        
+    }
+    #endregion
+
+    #region  Input
+    public InputActions inputActions;
+    #endregion
+
+    #region Loading
+    private List<AsyncOperation> runningLoadingOperations;
+    public void loadScene(int index){
+        AsyncOperation op = SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
+        op.allowSceneActivation = false;
+        runningLoadingOperations.Add(op);
+    }
+    public void unloadScene(int index){
+        runningLoadingOperations.Add(SceneManager.UnloadSceneAsync(index, UnloadSceneOptions.None));
+    }
+
+    public void finishLoadOperation(){
+        foreach (var loadingOperation in runningLoadingOperations){
+            loadingOperation.allowSceneActivation = true;
+        }
+    }
+
+    public float percentageCompleted(){
+        float unscaledProgress = 0;
+        foreach (AsyncOperation loadOperation in runningLoadingOperations)
+            unscaledProgress += loadOperation.progress;
+        return unscaledProgress / runningLoadingOperations.Count; //scale the progress by how many operations there are
+    }
+    #endregion
+
+
+
+}
