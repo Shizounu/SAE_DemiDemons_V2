6b1eb4a189a42fa2446c86b29a3551c5165c9cd0
Shizounu <erik@simon-potsdam.de>
2023-01-23 16:24:35 +0100

    Updated UI further

--- Assets/Scripts/UI_SectionSlider.cs
+++ Assets/Scripts/UI_SectionSlider.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class UI_SectionSlider : MonoBehaviour
 {
+    [Header("Audio editing")]
+    public AudioMixer audioMixer;
+    public string PropertyName = "Main Volume";
+    public Vector2 audioRange = new Vector2(-60, 20);
+    public void setVolume(){
+        audioMixer.SetFloat(PropertyName, Mathf.Lerp(audioRange.x, audioRange.y, (float)Index / 10));
+    }
+
+    [Header("Slider Stuff")]
     [SerializeField, Range(0,9)] private int Index = 0;
     public UnityEngine.Events.UnityEvent OnValueChange;
     public void IncreaseValue(){
@@ -31,5 +41,7 @@ public class UI_SectionSlider : MonoBehaviour
     private void OnDisable() {
         OnValueChange.RemoveListener(UpdateVisuals);
     }
+
+    
 
 }
